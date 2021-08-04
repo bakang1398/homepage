584707742d6b2c41254330ce6088e86691c34355
bakang1398 <83421867+bakang1398@users.noreply.github.com>
2021-08-05 00:25:04 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,125 @@
+const navBar = document.querySelector('#navBar');
+
+const logo = document.createElement('h3');
+logo.classList.add('logo');
+logo.textContent = 'TASK & TIME';
+
+const menuBar = document.createElement('ul');
+menuBar.classList.add('menu');
+const homeTab = document.createElement('li')
+const toDo = document.createElement('li');
+const calender = document.createElement('li');
+
+homeTab.textContent = 'HOME';
+toDo.textContent = 'TO DO';
+calender.textContent = 'CALENDER';
+
+menuBar.appendChild(logo);
+menuBar.appendChild(homeTab);
+menuBar.appendChild(toDo);
+menuBar.appendChild(calender);
+
+navBar.appendChild(menuBar);
+
+let bannerImg = document.createElement('img');
+bannerImg.classList.add('banner');
+bannerImg.src = 'images/planning.jpg';
+
+document.body.appendChild(bannerImg);
+
+let div = document.createElement('div');
+div.classList.add('header');
+const header = document.createElement('h1');
+header.textContent = '';
+
+div.appendChild(header);
+document.body.appendChild(div);
+
+const advantages = document.createElement('h2');
+advantages.classList.add('adv');
+advantages.textContent = 'ADVANTAGES OF A TO DO LIST:';
+
+document.body.appendChild(advantages);
+
+let container = document.createElement('div');
+container.classList.add('container');
+
+let content1 = document.createElement('div');
+content1.classList.add('content');
+
+let heading1 = document.createElement('h3');
+heading1.classList.add('heading');
+heading1.textContent = 'A to-do list doesn’t forget!';
+
+let text1 = document.createElement('p');
+text1.classList.add('texts');
+text1.textContent = 'Your brain is not the most efficient memory tool and will only trust systems that it knows works. Good memory recall is as simple as finding those things that will jog your brain at the time it needs to remember. Having a written list helps us remember when things have do be done so we do not miss anything.';
+
+content1.appendChild(heading1);
+content1.appendChild(text1);
+container.appendChild(content1);
+
+let content2 = document.createElement('div');
+content2.classList.add('content');
+
+let heading2 = document.createElement('h3');
+heading2.classList.add('heading');
+heading2.textContent = 'A to-do list helps you set priorities!';
+
+let text2 = document.createElement('p');
+text2.classList.add('texts');
+text2.textContent = 'Making a to-do list is an important first step but prioritizing that list ensures that you focus on the most important items rather than giving in to the temptation of working on less important items because they may stand out more or because they are easier to do.';
+
+content2.appendChild(heading2);
+content2.appendChild(text2);
+container.appendChild(content2);
+
+let content3 = document.createElement('div');
+content3.classList.add('content');
+
+let heading3 = document.createElement('h3');
+heading3.classList.add('heading');
+heading3.textContent = 'A to-do list lets you coordinate similar tasks!';
+
+let text3 = document.createElement('p');
+text3.classList.add('texts');
+text3.textContent = 'A to-do list helps us to avoid repetition of labour. For example, if we have to deliver a document at an office and collect a document from another office which is on the same block, both these tasks can be done together.';
+
+content3.appendChild(heading3);
+content3.appendChild(text3);
+container.appendChild(content3);
+
+let content4 = document.createElement('div');
+content4.classList.add('content');
+
+let heading4 = document.createElement('h3');
+heading4.classList.add('heading');
+heading4.textContent = 'A to-do list tracks your progress!';
+
+let text4 = document.createElement('p');
+text4.classList.add('texts');
+text4.textContent = 'Using a to-do list enables you to mark off the tasks you have completed. At the end of the day, when you look at the list, it will give you a sense of accomplishment and satisfaction. It might also have the effect of waking you up if nothing has been marked completed.';
+
+content4.appendChild(heading4);
+content4.appendChild(text4);
+container.appendChild(content4);
+
+let content5 = document.createElement('div');
+content5.classList.add('content');
+
+let heading5 = document.createElement('h3');
+heading5.classList.add('heading');
+heading5.textContent = 'A to-do list makes it easy to carry-over tasks!';
+
+let text5 = document.createElement('p');
+text5.classList.add('texts');
+text5.textContent = 'If anything remains incomplete at the end of the day, it can be carried over to tomorrow’s list. This is an easy way of preparing a to-do list for the next day; by examining the to-do list of today and carrying forward any task that is incomplete.';
+
+content5.appendChild(heading5);
+content5.appendChild(text5);
+container.appendChild(content5);
+
+document.body.appendChild(container);
+
+
+
